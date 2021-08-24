8011ef7582a644b78feaf3df8d30d7e59fd4d734
shariqanwar20 <shariq.anwar@nixorcollege.edu.pk>
2021-08-24 11:13:33 +0500

    added the functionality of todoList smart contract on frontend with web3

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,159 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import { TODO_LIST_ADDRESS, TODO_LIST_ABI } from "./config";
+import "./App.css";
+import Web3 from "web3";
+import { Button, Form } from "react-bootstrap";
+declare let window: any;
 
 function App() {
+  const [account, setAccount] = useState("");
+  const [tasks, setTasks] = useState<any>([]);
+  const [todoListContract, setTodoListContract] = useState<any>();
+  const [loading, setLoading] = useState(false);
+  const taskValue = useRef<HTMLInputElement>(null);
+  const checkbox = useRef<HTMLInputElement>(null);
+
+  const createTask = (content: string) => {
+    setLoading(true);
+    todoListContract.methods
+      .createTask(content)
+      .send({ from: account })
+      .once("receipt", (receipt: any) => {
+        console.log("receipt :", receipt);
+        
+        getTasks().then(() => {
+          setLoading(false);
+        });
+      });
+  };
+
+  const taskCompleted = (taskId: string) => {
+    setLoading(true);
+    todoListContract.methods
+      .toggleCompleted(taskId)
+      .send({ from: account })
+      .once("receipt", (receipt: any) => {
+        console.log(receipt);
+        getTasks().then(() => {
+          setLoading(false);
+        });
+      });
+  };
+
+  const getTasks = async () => {
+    if (todoListContract !== undefined) {
+      setLoading(true);
+      /* fetch the total number of tasks stored in our ganache blockchain */
+      const taskCount = await todoListContract.methods.taskCount().call();
+
+      /* get all tasks from blockchain */
+      let currentTasks = []
+      for (let i = 1; i <= taskCount; i++) {
+        const task = await todoListContract.methods.tasks(i).call();
+        console.log("Task => ", task);
+        
+        currentTasks.push(task)
+      }
+      // console.log("tasks=>", currentTasks);
+      
+      setTasks(currentTasks)
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const getAccountAddresses = async () => {
+      /* this is to ensure metamask is installed in browser */
+      if (window.ethereum !== undefined) {
+        /* metamask would try to connect with the Dapp running in yourbrowser */
+        await window.ethereum.enable();
+
+        /* create an instance of Web3 on localhost 8545(it is a default ethereum port)
+           switch the network in metamask to 8545
+        */
+        const web3 = new Web3("http://localhost:8545");
+
+        // const taskCount = await todoList.methods.taskCount().call();
+        // console.log(taskCount);
+
+        /* get user's wallet address from metamask wallet */
+        const accounts = await web3.eth.getAccounts();
+        setAccount(accounts[0]);
+
+        /* initialize our contract with contract address(copy this from ganache) and contract's ABI */
+        const todoList = new web3.eth.Contract(
+          TODO_LIST_ABI,
+          TODO_LIST_ADDRESS
+        );
+        setTodoListContract(todoList);
+
+        await getTasks();
+      } else {
+        alert("Install metamask");
+      }
+    };
+
+    getAccountAddresses();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-12 d-flex justify-content-center">
+            {loading ? (
+              <div className="text-center">
+                <p className="text-center">Loading...</p>
+              </div>
+            ) : (
+            <div id="content">
+              <Form
+                onSubmit={(event) => {
+                  event.preventDefault();
+                  console.log(taskValue.current?.value);
+                  
+                  createTask(taskValue.current?.value!);
+                }}
+              >
+                <Form.Group
+                  className="my-3 flex flex-wrap"
+                  controlId="formBasicPassword"
+                >
+                  <Form.Label>Task Name</Form.Label>
+                  <Form.Control type="text" placeholder="Add Task..." ref={taskValue} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+              <ul id="taskList" className="list-unstyled">
+                {tasks.map((task: any, key: number) => {
+                  return (
+                    <div className="taskTemplate checkbox" key={key}>
+                      <label>
+                        <input
+                          type="checkbox"
+                          name={task.id}
+                          defaultChecked={task.completed}
+                          ref={checkbox}
+                          onClick={(event) => {
+                            taskCompleted(task.id);
+                          }}
+                        />
+                        <span className="content text-black px-3">{task.content}</span>
+                      </label>
+                    </div>
+                  );
+                })}
+              </ul>
+              {console.log(tasks)}
+              {/* <ul id="completedTaskList" className="list-unstyled">
+              </ul> */}
+            </div>
+             )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
